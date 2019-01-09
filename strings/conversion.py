68a91b37233d6529bf0065bdd64d15b3af858c1f
Colin <tresscd@gmail.com>
2019-01-09 14:11:57 -0500

    Add int to str function.

--- strings/conversion.py
+++ strings/conversion.py
@@ -11,5 +11,22 @@ def str_to_int(str):
 print(str_to_int("12345"))
 
 
-def int_to_str(int):
+def int_to_str(x):
     new_str = ''
+    digits = "0123456789"
+    is_negative = False
+
+    if x < 0:
+        x = -x
+        is_negative = True
+
+    while x > 0:
+        new_str = digits[x % 10] + new_str
+        x //= 10
+
+    if is_negative:
+        new_str = "-" + new_str
+
+    return new_str
+
+print(int_to_str(-2303))
